be7c3f195aab901e847253c888a3227c6813ea95
KiralyTamas <tomaszpest@gmail.com>
2021-09-16 14:52:22 +0200

    Az leső feladat kész :)

--- js/main.js
+++ js/main.js
@@ -4,20 +4,23 @@ let firstButton = document.getElementById("firstButton");
 let animalName = document.getElementById("animal").value;
 let description = document.getElementById("description").value;
 let price = document.getElementById("price").value;
-let startFirst = firstFactory(animalName, description, price); //Valahol itt lesz a probléma
-let array=[animalName, description, price]
-function firstFactory(animalName, description, price) {
-  return {
-    neve: animalName,
-    leiras: description,
-    osszeg: price,
-    content: document.getElementById("content"),
-    currency: "EUR",
-    print: function () {
-      content.innerHTML = this.neve + this.leiras + this.osszeg + this.currency;
-    }
+class firstFactory {
+  constructor(animalName, description, price) {
+    firstButton = document.getElementById("firstButton");
+    animalName = document.getElementById("animal").value;
+    description = document.getElementById("description").value;
+    price = document.getElementById("price").value;
+    this.neve = animalName;
+    this.leiras = description;
+    this.osszeg = price;
+    this.content = document.getElementById("content");
+    this.currency = "EUR";
+    this.print = function () {
+      content.innerHTML = "A kedvenc állatom a " + this.neve + ", mert " + this.leiras + ". Egy ilyen kutyusnak az összege: " + this.osszeg + this.currency+".";
+    };
   }
 }
-function start(){
-firstButton.addEventListener("click", startFirst.print(array));}
+function start() {
+  firstButton.addEventListener("click", () => new firstFactory(animalName, description, price).print());
+}
 start();
