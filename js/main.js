7c91baf6478ebc3ef0f53c6a0a184ae5c6851366
KiralyTamas <tomaszpest@gmail.com>
2021-09-17 16:13:15 +0200

    AHOGY KI KELL NÉZNIE

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 // A első feladat, hogy egy Gyártó-fügvényt készítsünk
 
-let firstButton = document.getElementById("firstButton");
-let animalName = document.getElementById("animal").value;
-let description = document.getElementById("description").value;
-let price = document.getElementById("price").value;
+firstButton = document.getElementById("firstButton");
 class firstFactory {
   constructor(animalName, description, price) {
-    firstButton = document.getElementById("firstButton");
     animalName = document.getElementById("animal").value;
     description = document.getElementById("description").value;
     price = document.getElementById("price").value;
@@ -16,11 +12,11 @@ class firstFactory {
     this.content = document.getElementById("content");
     this.currency = "EUR";
     this.print = function () {
-      content.innerHTML = "A kedvenc állatom a " + this.neve + ", mert " + this.leiras + ". Egy ilyen kutyusnak az összege: " + this.osszeg + " " + this.currency+".";
+      content.innerHTML = "A kedvenc állatom a " + this.neve + ", " + this.leiras + ". Egy ilyen kutyusnak az összege: " + this.osszeg + " " + this.currency+".";
     };
   }
 }
 function start() {
-  firstButton.addEventListener("click", () => new firstFactory(animalName, description, price).print());
+  firstButton.addEventListener("click", () => new firstFactory(this.animalName, description, price).print());
 }
 start();
