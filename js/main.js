50c0b31bf0864bb171559101c523b0b68e73d67e
Király Tamás <tomaszpest@gmail.com>
2021-09-18 12:34:16 +0200

    Öröklődés folyamatban

--- js/main.js
+++ js/main.js
@@ -1,21 +1,51 @@
 // A első feladat, hogy egy Gyártó-fügvényt készítsünk
 
 firstButton = document.getElementById("firstButton");
-class firstFactory {
-  constructor(animalName, description, price) {
-    animalName = document.getElementById("animal").value;
+//Itt kezdődik a szülő konstruktor kódja
+class FirstFactory {
+  constructor(animalClass, description, price) {
+    animalClass = document.getElementById("animal").value;
     description = document.getElementById("description").value;
     price = document.getElementById("price").value;
-    this.neve = animalName;
+    this.neve = animalClass;
     this.leiras = description;
     this.osszeg = price;
     this.content = document.getElementById("content");
     this.currency = "EUR";
-    this.print = function () {
-      content.innerHTML = "A kedvenc állatom a " + this.neve + ", " + this.leiras + ". Egy ilyen kutyusnak az összege: " + this.osszeg + " " + this.currency+".";
-    };
+  }
+};
+//Itt található a FirstFactory prototipus fügvénye
+FirstFactory.prototype.print = function () {
+  content.innerHTML = "A kedvenc állatom a " + this.neve + ", " + this.leiras + ". Egy ilyen kutyusnak az összege: " + this.osszeg + " " + this.currency + ".";
+};
+//Itt található az eseményfigyelő, amit a firstButton gombra illesztettem rá.
+let actions = function () {
+  new FirstFactory(this.animalClass, description, price).print();
+  new FirstChild(this.animalClass, age, color).family();
+
+};
+(function () {
+  firstButton.addEventListener("click", actions);
+})();
+// Itt kezdődik a gyerek konstruktor kódja
+
+class FirstChild {
+  constructor(animalClass, age, color) {
+    new FirstFactory.call(this, animalClass)
+    age = document.getElementById("age").value;
+    color = document.getElementById("color").value;
+    this.content2 = document.getElementById("content2");
+    this.age = age;
+    this.color = color;
   }
 }
-(function () {
-  firstButton.addEventListener("click", () => new firstFactory(this.animalName, description, price).print());
-})();
+FirstChild.prototype = Object.create(FirstFactory.prototype);
+Object.defineProperty(FirstChild.prototype, 'family', {
+  value: FirstChild,
+  enumerable: false,
+  writable: true
+}
+);
+FirstChild.prototype.family = function () {
+  content2.innerHTML = "djfjgndfsgsjdf" + this.animalClass + "dff" + this.age + "dgdf" + this.color;
+};
